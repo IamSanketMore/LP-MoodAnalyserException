bf21b6ef69d9793412a14ebd6befa1d5787b8474
IamSanketMore <sanketmore.999.m@gmail.com>
2021-02-24 00:46:09 +0530

    UC2- in Junit test cases Handle Mood analayser Null Point Exception

--- test/java/MoodAnalyserTest.java
+++ test/java/MoodAnalyserTest.java
@@ -1,29 +1,41 @@
 import org.junit.Assert;
 import org.junit.Test;
-
+import java.lang.Exception;
 public class MoodAnalyserTest
 {
     @Test
     public void givenMessageWhenSad_ShouldReturnSad()
     {
-        MoodAnalyser moodAnalyser = new MoodAnalyser("This is a Sad Message");
-        String mood = moodAnalyser.analyseMood();
-        Assert.assertEquals("SAD",mood);
+        try {
+            MoodAnalyser moodAnalyser = new MoodAnalyser("This is a Sad Message");
+            String mood = moodAnalyser.analyseMood();
+            Assert.assertEquals("SAD", mood);
+        }
+        catch(MoodAnalyserException e)
+        {
+            System.out.println("Wrong Input Type");
+        }
     }
     @Test
-    public void givenMessageWhenNotSad_ShouldReturnHappy()
-    {
-        MoodAnalyser moodAnalyser = new MoodAnalyser("This is a Happy Message");
-        String mood = moodAnalyser.analyseMood();
-        Assert.assertEquals("HAPPY",mood);
+    public void givenMessageWhenNotSad_ShouldReturnHappy() {
+        try {
+            MoodAnalyser moodAnalyser = new MoodAnalyser("This is a Happy Message");
+            String mood = moodAnalyser.analyseMood();
+            Assert.assertEquals("HAPPY", mood);
+        } catch (MoodAnalyserException e) {
+            System.out.println("Wrong Input Type");
+        }
     }
-
     @Test
     public void givenNullMood_ShouldReturnHappy()
     {
+        try{
         MoodAnalyser moodAnalyser = new MoodAnalyser(null);
         String mood = moodAnalyser.analyseMood();
         Assert.assertEquals("HAPPY",mood);
+        } catch (MoodAnalyserException e) {
+            System.out.println("Wrong Input Type");
+        }
     }
 }
 
